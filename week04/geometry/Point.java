c87c0b7ce7bee76dc9f79bdb210fae53db75c2cd
Konul Abbasova <konul@Konuls-MacBook-Air.local>
2026-02-12 10:40:06 +0400

    Updated

---
+++ week04/geometry/Point.java
@@ -0,0 +1,25 @@
+package week04.geometry;
+
+public class Point {
+    private float x;
+    private float y;
+    public Point()
+    {
+        x= 0f;
+        y=0f;
+    }
+    public Point(float x, float y){
+        this.x=x;
+        this.y=y;
+    }
+    public Point(Point p){
+        this.x= p.x;
+        this.y=p.y;
+    }
+    public float getX(){
+        return this.x;
+    }
+    public void setx(float x){
+        this.x=x;
+    }
+}
